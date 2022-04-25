4fdcf618ce9677f7c5e62d84e8db00f3f5e406bf
juvenile <1050299953@qq.com>
2022-04-25 19:13:59 +0800

    update

--- parser/app_test.go
+++ parser/app_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
-	"github.com/teambition/swaggo/swagger"
+	"github.com/Collection-fork/swaggo/swagger"
 )
 
 func TestErrorPath(t *testing.T) {
